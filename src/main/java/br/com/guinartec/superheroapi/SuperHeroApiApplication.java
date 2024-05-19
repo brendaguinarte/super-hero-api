3194d38dd991a320a173d8a9a15b51ce70fdaa56
brenda.souza <brenda.sousa@superbid.net>
2024-05-18 21:09:11 -0300

    first commit

---
+++ src/main/java/br/com/guinartec/superheroapi/SuperHeroApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.guinartec.superheroapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperHeroApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperHeroApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/guinartec/superheroapi/SuperHeroApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.guinartec.superheroapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperHeroApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
